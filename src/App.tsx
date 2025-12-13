1ffeb6e2146f3faa28856f72e7004d2d0ff4f99b
kasukurthikiran <sk@konic.net>
2025-12-13 18:38:48 +0000

    chore: fix the app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600'>
         <h1 className='text-5xl font-bold text-white drop-shadow-lg'>Hi, I'm Saikiran</h1>
       </div>
+     
     </>
   )
 }
